GStreamer/gstreamer

tsdemux: reset the stream some more in the flushes
Now that we handle flushing in the FLUSH_STOP event we need to make
the streams ready if they were flushing.

gst/mpegtsdemux/tsdemux.c
@@ -1092,6 +1092,9 @@ gst_ts_demux_stream_flush (TSDemuxStream * stream)
   stream->fixed_dts = 0;
   stream->nb_pts_rollover = 0;
   stream->nb_dts_rollover = 0;
+  if (stream->flow_return == GST_FLOW_FLUSHING) {
+    stream->flow_return = GST_FLOW_OK;
+  }
 }
 
 static void

ylatuya/gst-plugins-bad

Conflicts:
	gst/mpegtsdemux/tsdemux.c

gst/mpegtsdemux/tsdemux.c
@@ -1111,6 +1111,9 @@ gst_ts_demux_stream_flush (TSDemuxStream * stream)
   stream->nb_pts_rollover = 0;
   stream->nb_dts_rollover = 0;
   stream->continuity_counter = CONTINUITY_UNSET;
+  if (stream->flow_return == GST_FLOW_WRONG_STATE) {
+    stream->flow_return = GST_FLOW_OK;
+  }
 }
 
 static void